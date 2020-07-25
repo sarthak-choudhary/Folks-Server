81516931391d612896c4c2d3c2c3d8a63fc59c60
anshalshukla <shukla.anshal85@gmail.com>
2020-07-26 01:21:50 +0530

    event model updated.

--- db/models/events.go
+++ db/models/events.go
@@ -2,17 +2,19 @@ package models
 
 import (
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type event struct {
-	ID           string    `json:"id" required:"true"`
-	Name         string    `json:"name" required:"true"`
-	Description  string    `json:"description" required:"true"`
-	Destination  string    `json:"destination"`
-	Datetime     time.Time `json:"datetime"`
-	HostedBy     string    `json:"hosted_by" required:"true"`
-	Participants string    `json:"participants"`
-	Pictures     []string  `json:"pictures"`
+	ID           primitive.ObjectID   `bson:"_id" json:"id" required:"true"`
+	Name         string               `bson:"name" json:"name" required:"true"`
+	Description  string               `bson:"description" json:"description,omitempty"`
+	Destination  string               `bson:"destination" json:"destination,omitempty"`
+	Datetime     time.Time            `bson:"datetime,omitempty" json:"datetime,omitempty"`
+	HostedBy     primitive.ObjectID   `bson:"hostedBy" json:"hostedBy" required:"true"`
+	Participants []primitive.ObjectID `bson:"participants,omitempty" json:"participants,omitempty"`
+	PicturesUrls []string             `bson:"picturesUrls,omitempty" json:"picturesUrls,omitempty"`
 }
 
 type events []event
