579e6349e67c5f60f7c79b32dcb24d868b9cb097
sarthak choudhary <f20190112@pilani.bits-pilani.ac.in>
2020-09-26 01:39:09 +0530

    squads model added

---
+++ db/models/squad.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+//Squad model
+type Squad struct {
+	ID          primitive.ObjectID   `bson:"_id" json:"_id" required:"true"`
+	Name        string               `bson:"name" json:"name" required:"true"`
+	Description string               `bson:"description,omitempty" json:"description,omitempty"`
+	GroupImages []string             `bson:"groupImages,omitempty" json:"groupImages,omitempty"`
+	Admins      []primitive.ObjectID `bson:"admins" json:"admins" required:"true"`
+	Members     []primitive.ObjectID `bson:"members,omitempty" json:"members,omitempty"`
+	InvitesSent []primitive.ObjectID `bson:"invitesSent,omitempty" json:"invitesSent,omitempty"`
+}
