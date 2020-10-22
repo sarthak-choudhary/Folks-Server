0e2def8f534f6d59d4d8a103984f88c6df3e8145
Anshal Shukla <shukla.anshal85@gmail.com>
2020-10-22 22:00:35 +0530

    Containerized db removed.

--- db/conn.go
+++ db/conn.go
@@ -17,7 +17,6 @@ type MongoDB struct {
 
 // ConnectDB connects to the database
 func ConnectDB() MongoDB {
-	// Change mongo ApplyURI -> "mongodb://db:27017/folks" to run with docker
 	// Change mongo ApplyURI -> "mongodb://localhost:27017/folks" to run locally
 	// Change mongo ApplyURI -> "mongodb+srv://folks:folks@cluster0.9up09.mongodb.net/<dbname>?retryWrites=true&w=majority" to run on mongodb atlas
 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb+srv://folks:folks@cluster0.9up09.mongodb.net/<dbname>?retryWrites=true&w=majority"))
